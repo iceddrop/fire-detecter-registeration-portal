34b0eaa075549f470e0bc66dbf3bf02e99b865ee
iceddrop <93996532+iceddrop@users.noreply.github.com>
2025-03-09 23:03:35 +0100

    working dashboard

--- src/component/AppNav/AppNav.jsx
+++ src/component/AppNav/AppNav.jsx
@@ -1,9 +1,18 @@
+import { Dropdown } from "flowbite-react";
+import { FaChevronDown,FaChevronRight  } from "react-icons/fa";
+import {useState} from "react";
 const AppNav = () => {
-    return (
-      <header className="bg-blue-600 text-white p-4 shadow-md">
-        <h1 className="text-lg font-semibold">My App</h1>
-      </header>
-    );
-  };
-  
-  export default AppNav;
+  const [dropdownTriggered, setDropdownTriggered] = useState(false);
+  return (
+    <header className="bg-[#002244] text-white p-4 shadow-md flex w-full justify-end">
+      <Dropdown label="" dismissOnClick={false} onClick={() => setDropdownTriggered(!dropdownTriggered)} className="cursor-pointer"  renderTrigger={() => <span className="flex items-center cursor-pointer">My custom trigger {dropdownTriggered ? <FaChevronDown className="ml-2"/> : <FaChevronRight className="ml-2"/>}</span>}>
+      <Dropdown.Item>Dashboard</Dropdown.Item>
+      <Dropdown.Item>Settings</Dropdown.Item>
+      <Dropdown.Item>Earnings</Dropdown.Item>
+      <Dropdown.Item>Sign out</Dropdown.Item>
+    </Dropdown>
+    </header>
+  );
+};
+
+export default AppNav;
--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,9 +1,28 @@
+import { FaTimes } from "react-icons/fa";
+import { IoHomeOutline } from "react-icons/io5";
+import { GrAnalytics } from "react-icons/gr";
+import { GoPerson } from "react-icons/go";
+import { LiaHandsHelpingSolid } from "react-icons/lia";
+import { IoIosLogOut } from "react-icons/io";
+
 const Sidebar = () => {
-    return (
-      <header className="bg-blue-600 text-white p-4 shadow-md">
-        <h1 className="text-lg font-semibold">Sidebar</h1>
-      </header>
-    );
-  };
-  
-  export default Sidebar;
+  return (
+    <header className="bg-[#002244] text-white w-[20%] pt-6 px-4 shadow-md">
+      <div className="w-full flex justify-between md:justify-center items-center">
+        <span className="brand-text-one self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+          Fire<span className="brand-text-two text-red-600 ">Eyes</span>
+        </span>
+        <FaTimes className="md:hidden"/>
+      </div>
+      <ul className="w-full flex flex-col justify-around items-center h-[70%] pt-10">
+        <li className="flex items-center"><IoHomeOutline className="mr-2"/>Home</li>
+        <li className="flex items-center"><GrAnalytics className="mr-2"/>Analytics</li>
+        <li className="flex items-center"><GoPerson className="mr-2"/>Profile</li>
+        <li className="flex items-center"><LiaHandsHelpingSolid className="mr-2"/>Help Desk</li>
+        <li className="flex items-center"><IoIosLogOut className="mr-2"/>Logout</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Sidebar;
